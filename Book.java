9b489944b89a8c9305b5a8436e8ffa38b8267056
topia123 <violet.846123@gmail.com>
2021-07-31 18:06:14 +0900

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,8 @@
+public class Book extends TangibleAsset{
+	private String isbn;
+	public Bool (String name, int price, String color, Stirng isbn){
+		super(name, price, color);
+		tis. isbn = isbn;
+	}
+	public String getisbn(){retirm this.isbn;}
+}
